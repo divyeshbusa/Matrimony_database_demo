7db5d7e16aa01eebbb16f1f11bf809d64968c3e0
busa <200540107028@darshan.ac.in>
2023-02-02 23:17:59 +0530

    dashboard update

--- lib/Models/user_list_model.dart
+++ lib/Models/user_list_model.dart
@@ -1,14 +1,28 @@
 class UserModel {
-  int ? UserID1;
-
-  int ? get UserID => UserID1;
-
-  set UserID(int ? UserID) {
-    UserID1 = UserID;
-  }
+  int? UserID1;
   late String UserName1;
   late int PhoneNo1;
   late bool IsFavourite1;
+  late String DOB1;
+  late int Cityid1;
+  late String Photo1;
+  late int GenderID1;
+
+  UserModel(
+      {required this.UserName1,
+      required this.PhoneNo1,
+      required this.Cityid1,
+      required this.Photo1,
+      required this.GenderID1,
+      required this.IsFavourite1,
+      required this.DOB1});
+
+  int? get UserID => UserID1;
+
+  set UserID(int? UserID) {
+    UserID1 = UserID;
+  }
+
 
   bool get IsFavourite => IsFavourite1;
 
@@ -17,29 +31,14 @@ class UserModel {
   }
 
 
-  late String DOB1;
-
-
-  UserModel(
-      {
-        required this.UserName1,
-      required this.PhoneNo1,
-      required this.Cityid1,
-      required this.Photo1,
-      required this.GenderID1,
-      required this.IsFavourite1,
-      required this.DOB1});
-
-
   int get PhoneNo => PhoneNo1;
 
   set PhoneNo(int PhoneNo) {
     PhoneNo1 = PhoneNo;
   }
-  late int Cityid1;
 
-  late String Photo1;
-  late int GenderID1;
+
+
 
   int get Genderid => GenderID1;
 
@@ -59,22 +58,15 @@ class UserModel {
     Cityid1 = Cityid;
   }
 
-
-
-
   String get UserName => UserName1;
 
   set UserName(String UserName) {
     UserName1 = UserName;
   }
 
-
-
   String get Photo => Photo1;
 
   set Photo(String Photo) {
     Photo1 = Photo;
   }
-
-
 }
